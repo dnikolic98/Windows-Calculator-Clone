62fd95f993c4bfe04c4f18551176b9de06979c5e
Dario <dn50824@fer.hr>
2020-03-07 12:15:37 +0100

    minor changes

--- resources/js/keyboardMapping.js
+++ resources/js/keyboardMapping.js
@@ -1,54 +1,54 @@
 document.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 96 || event.keyCode === 48) {
-        document.getElementById("button0").click();
-    }
-    if (event.keyCode === 97 || event.keyCode === 49) {
-        document.getElementById("button1").click();
-    }
-    if (event.keyCode === 98 || event.keyCode === 50) {
-        document.getElementById("button2").click();
-    }
-    if (event.keyCode === 99 || event.keyCode === 51) {
-        document.getElementById("button3").click();
-    }
-    if (event.keyCode === 100 || event.keyCode === 52) {
-        document.getElementById("button4").click();
-    }
-    if (event.keyCode === 101 || event.keyCode === 53) {
-        document.getElementById("button5").click();
-    }
-    if (event.keyCode === 102 || event.keyCode === 54) {
-        document.getElementById("button6").click();
-    }
-    if (event.keyCode === 103 || event.keyCode === 55) {
-        document.getElementById("button7").click();
-    }
-    if (event.keyCode === 104 || event.keyCode === 56) {
-        document.getElementById("button8").click();
-    }
-    if (event.keyCode === 105 || event.keyCode === 57) {
-        document.getElementById("button9").click();
-    }
-    if (event.keyCode === 8) {
-        document.getElementById("buttonBks").click();
-    }
-    if (event.keyCode === 13) {
-        document.getElementById("buttonEq").click();
-    }
-    if (event.keyCode === 107) {
-        document.getElementById("button+").click();
-    }
-    if (event.keyCode === 106) {
-        document.getElementById("button*").click();
-    }
-    if (event.keyCode === 110  || event.keyCode === 188  || event.keyCode === 190) {
-        document.getElementById("buttonDec").click();
-    }
-    if (event.keyCode === 109) {
-        document.getElementById("button-").click();
-    }
-    if (event.keyCode === 111) {
-        document.getElementById("button/").click();
-    }
+  event.preventDefault();
+  if (event.keyCode === 96 || event.keyCode === 48) {
+    document.getElementById("button0").click();
+  }
+  if (event.keyCode === 97 || event.keyCode === 49) {
+    document.getElementById("button1").click();
+  }
+  if (event.keyCode === 98 || event.keyCode === 50) {
+    document.getElementById("button2").click();
+  }
+  if (event.keyCode === 99 || event.keyCode === 51) {
+    document.getElementById("button3").click();
+  }
+  if (event.keyCode === 100 || event.keyCode === 52) {
+    document.getElementById("button4").click();
+  }
+  if (event.keyCode === 101 || event.keyCode === 53) {
+    document.getElementById("button5").click();
+  }
+  if (event.keyCode === 102 || event.keyCode === 54) {
+    document.getElementById("button6").click();
+  }
+  if (event.keyCode === 103 || event.keyCode === 55) {
+    document.getElementById("button7").click();
+  }
+  if (event.keyCode === 104 || event.keyCode === 56) {
+    document.getElementById("button8").click();
+  }
+  if (event.keyCode === 105 || event.keyCode === 57) {
+    document.getElementById("button9").click();
+  }
+  if (event.keyCode === 8) {
+    document.getElementById("buttonBks").click();
+  }
+  if (event.keyCode === 13) {
+    document.getElementById("buttonEq").click();
+  }
+  if (event.keyCode === 107) {
+    document.getElementById("button+").click();
+  }
+  if (event.keyCode === 106) {
+    document.getElementById("button*").click();
+  }
+  if (event.keyCode === 110 || event.keyCode === 188 || event.keyCode === 190) {
+    document.getElementById("buttonDec").click();
+  }
+  if (event.keyCode === 109) {
+    document.getElementById("button-").click();
+  }
+  if (event.keyCode === 111) {
+    document.getElementById("button/").click();
+  }
 });
--- resources/js/logic.js
+++ resources/js/logic.js
@@ -1,103 +1,124 @@
-var scrReset=0;
-var result=0;
-var isDec=0;
-var logActive=0;
+var scrReset = 0;
+var result = 0;
+var isDec = 0;
+var logActive = 0;
 var shwLogActive = 0;
 var operatorActive = 0;
 
 function OnNumber(value) {
   operatorActive = 0;
-  if(document.getElementById('mainDisplay').innerHTML.trim()=="0" || scrReset==1) {
-    scrReset=0;
-    document.getElementById('mainDisplay').innerHTML = "";
-    if(value=="."){
-      document.getElementById('mainDisplay').innerHTML = "0";
+  if (
+    document.getElementById("mainDisplay").innerHTML.trim() == "0" ||
+    scrReset == 1
+  ) {
+    scrReset = 0;
+    document.getElementById("mainDisplay").innerHTML = "";
+    if (value == ".") {
+      document.getElementById("mainDisplay").innerHTML = "0";
     }
   }
-  if(value!="." || isDec==0){
-    result=0;
-    document.getElementById('mainDisplay').innerHTML += value;
+  if (value != "." || isDec == 0) {
+    result = 0;
+    document.getElementById("mainDisplay").innerHTML += value;
   }
-  if(value=="."){
-    isDec=1;
+  if (value == ".") {
+    isDec = 1;
   }
 }
 
 function OnOperator(value) {
-  scrReset=1;
-  result=1;
-  isDec=0;
-  if (document.getElementById('mainDisplay').innerHTML.slice(-1) == ".") {
-    document.getElementById('mainDisplay').innerHTML = document.getElementById('mainDisplay').innerHTML.slice(0, -1);
+  scrReset = 1;
+  result = 1;
+  isDec = 0;
+  if (document.getElementById("mainDisplay").innerHTML.slice(-1) == ".") {
+    document.getElementById("mainDisplay").innerHTML = document
+      .getElementById("mainDisplay")
+      .innerHTML.slice(0, -1);
   }
   if (operatorActive == 1) {
-    document.getElementById('secDisplay').innerHTML = (document.getElementById('secDisplay').innerHTML.slice(0,-2) + value + " ");
-  }else{
-  document.getElementById('secDisplay').innerHTML += document.getElementById('mainDisplay').innerHTML
-  document.getElementById('mainDisplay').innerHTML = eval(document.getElementById('secDisplay').innerHTML);
-  document.getElementById('secDisplay').innerHTML += (" " + value + " ");
-  operatorActive = 1;
+    document.getElementById("secDisplay").innerHTML =
+      document.getElementById("secDisplay").innerHTML.slice(0, -2) +
+      value +
+      " ";
+  } else {
+    document.getElementById("secDisplay").innerHTML += document.getElementById(
+      "mainDisplay"
+    ).innerHTML;
+    document.getElementById("mainDisplay").innerHTML = eval(
+      document.getElementById("secDisplay").innerHTML
+    );
+    document.getElementById("secDisplay").innerHTML += " " + value + " ";
+    operatorActive = 1;
   }
 }
 
-function OnEqual(){
-  scrReset=1;
-  result=1;
-  isDec=0;
-  document.getElementById('secDisplay').innerHTML += document.getElementById('mainDisplay').innerHTML;
-  var tempSec = document.getElementById('secDisplay').innerHTML;
-  document.getElementById('mainDisplay').innerHTML = eval(document.getElementById('secDisplay').innerHTML);
-  document.getElementById('secDisplay').innerHTML = ("");
-  var tempMain = document.getElementById('mainDisplay').innerHTML
-  if (document.getElementById('mainDisplay').innerHTML == "NaN") {
-    document.getElementById('mainDisplay').innerHTML = "Result is not defined";
+function OnEqual() {
+  scrReset = 1;
+  result = 1;
+  isDec = 0;
+  document.getElementById("secDisplay").innerHTML += document.getElementById(
+    "mainDisplay"
+  ).innerHTML;
+  var tempSec = document.getElementById("secDisplay").innerHTML;
+  document.getElementById("mainDisplay").innerHTML = eval(
+    document.getElementById("secDisplay").innerHTML
+  );
+  document.getElementById("secDisplay").innerHTML = "";
+  var tempMain = document.getElementById("mainDisplay").innerHTML;
+  if (document.getElementById("mainDisplay").innerHTML == "NaN") {
+    document.getElementById("mainDisplay").innerHTML = "Result is not defined";
   }
-  LogCreate(tempSec + " =",tempMain);
+  LogCreate(tempSec + " =", tempMain);
 }
 
 function OnNegate() {
-  var neg = Math.sign(eval(document.getElementById('mainDisplay').innerHTML))
-  if(neg==1){
-    document.getElementById('mainDisplay').innerHTML = "-" + document.getElementById('mainDisplay').innerHTML;
-  } else if (neg==-1) {
-    document.getElementById('mainDisplay').innerHTML = eval("-(" + document.getElementById('mainDisplay').innerHTML + ")")
+  var neg = Math.sign(eval(document.getElementById("mainDisplay").innerHTML));
+  if (neg == 1) {
+    document.getElementById("mainDisplay").innerHTML =
+      "-" + document.getElementById("mainDisplay").innerHTML;
+  } else if (neg == -1) {
+    document.getElementById("mainDisplay").innerHTML = eval(
+      "-(" + document.getElementById("mainDisplay").innerHTML + ")"
+    );
   }
 }
 
 function OnCE() {
-  document.getElementById('mainDisplay').innerHTML = "0";
-  scrReset=1;
+  document.getElementById("mainDisplay").innerHTML = "0";
+  scrReset = 1;
 }
 
 function OnClear() {
-  document.getElementById('secDisplay').innerHTML = "";
-  document.getElementById('mainDisplay').innerHTML = "0";
-  scrReset=1;
-  isDec=0;
+  document.getElementById("secDisplay").innerHTML = "";
+  document.getElementById("mainDisplay").innerHTML = "0";
+  scrReset = 1;
+  isDec = 0;
 }
 
 function OnBks() {
-  if (result==0) {
-    if (document.getElementById('mainDisplay').innerHTML.slice(-1) == ".") {
-      isDec=0;
+  if (result == 0) {
+    if (document.getElementById("mainDisplay").innerHTML.slice(-1) == ".") {
+      isDec = 0;
     }
-    document.getElementById('mainDisplay').innerHTML = document.getElementById('mainDisplay').innerHTML.slice(0, -1);
+    document.getElementById("mainDisplay").innerHTML = document
+      .getElementById("mainDisplay")
+      .innerHTML.slice(0, -1);
   }
-  if(document.getElementById('mainDisplay').innerHTML.trim() == ""){
-    document.getElementById('mainDisplay').innerHTML = "0";
+  if (document.getElementById("mainDisplay").innerHTML.trim() == "") {
+    document.getElementById("mainDisplay").innerHTML = "0";
   }
 }
 
-function LogCreate(secDisplay,mainDisplay) {
-  if(logActive==0){
+function LogCreate(secDisplay, mainDisplay) {
+  if (logActive == 0) {
     var clearLog = document.createElement("button");
     clearLog.className = "otherButton";
     clearLog.onclick = "clearLog()";
-    clearLog.id= "ClearLog";
-    clearLog.appendChild(document.createTextNode("Clear History"))
-    document.getElementById('TrashBar').appendChild(clearLog);
-    document.getElementById("noHis").innerHTML = ""
-    logActive=1;
+    clearLog.id = "ClearLog";
+    clearLog.appendChild(document.createTextNode("Clear History"));
+    document.getElementById("TrashBar").appendChild(clearLog);
+    document.getElementById("noHis").innerHTML = "";
+    logActive = 1;
   }
   var log = document.createElement("div");
   var secLog = document.createElement("div");
@@ -114,37 +135,41 @@ function LogCreate(secDisplay,mainDisplay) {
 
 function clearLog() {
   document.getElementById("History").innerHTML = "";
-  logActive=0;
+  logActive = 0;
 }
 
 //ne radi zbog nekog razloga
 function reloadLog(secLog) {
   document.getElementById("secDisplay").innerHTML = secLog;
-  document.getElementById("secDisplay").innerHTML = document.getElementById("secDisplay").innerHTML.slice(0, -1);
-  document.getElementById("mainDisplay").innerHTML = eval(document.getElementById("secDisplay").innerHTML);
-
+  document.getElementById("secDisplay").innerHTML = document
+    .getElementById("secDisplay")
+    .innerHTML.slice(0, -1);
+  document.getElementById("mainDisplay").innerHTML = eval(
+    document.getElementById("secDisplay").innerHTML
+  );
 }
 
 //napraviti slide from the bottom upwards
 function shwHideHistory() {
-  if (shwLogActive==0) {
+  if (shwLogActive == 0) {
     //$("#History").fadeIn();
-    document.getElementById("History").style.removeProperty('display');
-    document.getElementById("History").style.removeProperty('visibility');
-    document.getElementById("History").style.animation = "slideUp 0.2s ease-out 0s normal forwards";
-    shwLogActive=1;
-  }else {
-    document.getElementById("History").style.removeProperty('animation');
+    document.getElementById("History").style.removeProperty("display");
+    document.getElementById("History").style.removeProperty("visibility");
+    document.getElementById("History").style.animation =
+      "slideUp 0.2s ease-out 0s normal forwards";
+    shwLogActive = 1;
+  } else {
+    document.getElementById("History").style.removeProperty("animation");
     document.getElementById("History").style.visibility = "visible";
     $("#History").fadeOut();
-    shwLogActive=0;
+    shwLogActive = 0;
   }
 }
 
 $(function() {
-   $( "#Calculator" ).draggable();
- })
+  $("#Calculator").draggable();
+});
 
- $( function() {
-     $( "#Calculator" ).resizable();
-   } );
+$(function() {
+  $("#Calculator").resizable();
+});
